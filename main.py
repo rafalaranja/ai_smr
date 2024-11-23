b6107fae14ebe3396febc9573a4a1266226eda46
Rafael Santos <2211020@my.ipleiria.pt>
2024-11-23 01:09:59 +0000

    Download File Created
    We created the download file, and put the download function there, then imported it in the main.py

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-from pytubefix import YouTube
-from moviepy import AudioFileClip
-
-def download_audio(youtube_url):
-    try:
-        # Download YouTube video as audio-only
-        yt = YouTube(youtube_url)
-        stream = yt.streams.get_audio_only()
-        audio_file_path = stream.download(filename="audio.mp4")
-
-        # Convert to WAV for further processing
-        audio_clip = AudioFileClip(audio_file_path)
-        output_audio_path = "audio.wav"
-        audio_clip.write_audiofile(output_audio_path)
-
-        print(f"Audio downloaded and saved as {output_audio_path}")
-        return output_audio_path
-    except Exception as e:
-        print(f"Error in download_audio: {e}")
-        return None
+from download import download_audio
 
 # Main
 if __name__ == "__main__":
